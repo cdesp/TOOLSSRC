23ec6ed63e29b2f2f6ad7e2cc51d8b08b89d8b73
cdesp <cdesp72@gmail.com>
2023-02-11 15:59:49 +0200

    Newbrain Keyboard Mapper Helps to map NB keys to PC keyboard

---
+++ NBKeyMapNew/NBKeyMap.dpr
@@ -0,0 +1,14 @@
+program NBKeyMap;
+
+uses
+  Vcl.Forms,
+  NbKeymapMain in 'NbKeymapMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
